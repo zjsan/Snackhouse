cc6050af7fc6bff8c095bf9c4ef875907a6c9975
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-06-10 11:05:22 +0800

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,77 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial scale=1">
+		<link rel="stylesheet" href="style.css">
+		<title>Contact Us</title>
+	</head>
+	<body>
+		<div class="banner">
+			<div class="navbar">
+				<a href="home.php"><img src="Pictures/logo.png"class="logo"><a>
+				<ul>
+					<li> <a href="home.php">Home</a></li>
+					<li> <a href="menu.php">Menu</a></li>
+					<li> <a href="image.php">Image Gallery</a></li>
+					<li> <a href="aboutus.php">About Us</a></li>
+					<li> <a href="contactus.php">Contact Us</a></li>
+				</ul>
+			</div>
+
+			<div class="wrapper">
+				<h1>WE'D LIKE TO HEAR FROM YOU</h1>
+				<p>GET IN TOUCH WITH THE RIGHT PEOPLE AT SANDUNES SNACK HOUSE. IF YOU HAVE QUESTIONS OR FEEDBACKS ABOUT OUR SERVICES, PRODUCTS, PRICING, ETC. <br>
+				FEEL FREE TO LEAVE US A MESSAGE VIA OUR SOCIAL MEDIA ACCOUNTS.</p>
+				<div class="owners">
+					<div class="owner">
+						<div class="owner_img">
+							<img src="Pictures/owner1.jpg" alt="">
+						</div>
+						<h3>ELVIS DOMINGO</h3>
+						<p class="role">Owner</p>
+						<p>Social Media Acount</p>
+						<div class="social_media">
+							<a href="https://web.facebook.com/elvis.domingo.7"><img src="Pictures/facebook.jpg" alt=""></a>
+						</div>
+					</div>
+					<div class="owner">
+						<div class="owner_img">
+							<img src="Pictures/owner2.jpg" alt="">
+						</div>
+						<h3>REVELYN DOMINGO</h3>
+						<p class="role">Owner</p>
+						<p>Social Media Acount</p>
+						<div class="social_media">
+							<a href="https://web.facebook.com/profile.php?id=100077526902427"><img src="Pictures/facebook.jpg" alt=""></a>		
+						</div>
+					</div>
+					<div class="owner">
+						<div class="owner_img">
+							<img src="Pictures/owner3.jpg" alt="">
+						</div>
+						<h3>SHYLEEN DOMINGO</h3>
+						<p class="role">Owner</p>
+						<p>Social Media Acount</p>
+						<div class="social_media">
+							<a href="https://web.facebook.com/shyleen.domingo1"><img src="Pictures/facebook.jpg" alt=""></a>
+							<a href="https://www.instagram.com/shyleenkaye/?fbclid=IwAR2noogdo3glTlISMJE1VGJQVF-uPPY9w7wGNC1vmGyH_bdli4dsuRnMlzQ"><img src="Pictures/instagram.jpg" alt=""></a>			
+						</div>
+					</div>					
+					<div class="owner">
+						<div class="owner_img">
+							<img src="Pictures/owner4.jpg" alt="">
+						</div>
+						<h3>DAVE DOMINGO</h3>
+						<p class="role">Owner</p>
+						<p>Social Media Acount</p>
+						<div class="social_media">
+							<a href="https://www.facebook.com/dave.domingo.169""><img src="Pictures/facebook.jpg" alt=""></a>
+							<a href="https://www.instagram.com/itsyoboydaveh/"><img src="Pictures/instagram.jpg" alt=""></a>			
+						</div>
+					</div>					
+				</div>
+			</div>		
+			
+		</div>
+	</body>
+</html>
