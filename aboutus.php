62e56e8e9174ada2aa5bb6b0f13c9ae912b21736
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-06-10 11:04:57 +0800

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial scale=1">
+		<link rel="stylesheet" href="style.css">
+		<title>About Us</title>
+	</head>
+	<body>
+		<div class="banner">
+			<div class="navbar">
+				<a href="home.php"><img src="Pictures/logo.png"class="logo"><a>
+				<ul>
+					<li> <a href="home.php">Home</a></li>
+					<li> <a href="menu.php">Menu</a></li>
+					<li> <a href="image.php">Image Gallery</a></li>
+					<li> <a href="aboutus.php">About Us</a></li>
+					<li> <a href="contactus.php">Contact Us</a></li>
+				</ul>
+			</div>	
+
+			<div class="about_us">
+				<div class="about_us_content">
+					<div class="about_us_content_head">
+						<h1>SAND DUNES SNACK HOUSE</h1>
+						<h4><em>Extraordinary food comes first</em></h4>
+					</div>
+					<div class="establishment_img">
+						<img src="Pictures/establishment.jpg" width="932px" height="474px">
+					</div>
+						<p>Since our modest beginnings in <em>2001</em> with a little space in Barangay 33-B La Paz and only served two options of foods, <em>paksiw and empanada</em>.
+							Our business's development has been enlivened with the energy to cook and serve solid Ilocano food.
+							<strong>SAND DUNES SNACK HOUSE</strong> is additionally home to the amazing Ilocos Empanada.In contrast to other eateries in Metro La Paz, <strong>SAND DUNES SNACK HOUSE</strong> was made with the explicit expectation to appear as something else.
+							Our menu highlights things that utilize the sound and fragrant flavors, which includes, lomi, miki, vegetable lumpia, empanada, and many more. <strong>SAND DUNES SNACK HOUSE</strong> is situated in the central part of Metro La Paz, just on the left side of the La Paz auditorium.
+							Extraordinary administration and a fun vibe keep local people returning and guests raving. In the celebration of Daklisan Festival of La Paz, <strong>SANDUNES SNACK HOUSE</strong> is the number one spot to have a good food with.
+						</p>
+				</div>
+			
+				<div class="map">
+					<div>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1046.240085333384!2d120.53656658811147!3d18.20290249922087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x436580122f99ae95!2zMTjCsDEyJzEwLjUiTiAxMjDCsDMyJzEzLjYiRQ!5e1!3m2!1sen!2sph!4v1646819506097!5m2!1sen!2sph"
+						width="100%" height="500px" style="border:;" allowfullscreen="" loading="lazy"></iframe>
+					</div>
+						<h1> Location </h1>
+						<p> Barangay 33-B La Paz Proper, Laoag City, Ilocos Norte </p>
+					<div class="info">
+						<h1>Store Hours:</h1>
+						<p>Monday - Sunday: 3Pm to 9Pm</p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</body>
+</html>
