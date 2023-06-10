c4f54726e0618fa9451d507fc07b25b0251d55cb
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-06-10 11:05:24 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial scale=1">
+		<link rel="stylesheet" href="style.css">
+		<title>Home Page</title>
+	</head>
+	<body>
+		<div class="banner">
+			<div class="navbar">
+				<a href="home.php"><img src="Pictures/logo.png"class="logo"></a>
+				<ul>
+					<li> <a href="home.php">Home</a></li>
+					<li> <a href="menu.php">Menu</a></li>
+					<li> <a href="image.php">Image Gallery</a></li>
+					<li> <a href="aboutus.php">About Us</a></li>
+					<li> <a href="contactus.php">Contact Us</a></li>
+				</ul>
+			</div>
+			
+			<div class="content">
+				<h1>SAND DUNES SNACK HOUSE</h1>
+				<p>Making time a good time by making food the good food</p>
+				<div class="button">
+				<a href="menu.php"><button type="button">EXPLORE</button></a>
+				</div>
+			</div>
+			
+		
+	</body>
+</html>
