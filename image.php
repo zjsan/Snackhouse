a8e477136e1ee21a359b86a993a6c53ec6d7c50a
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-06-10 11:05:26 +0800

    Create image.php

---
+++ image.php
@@ -0,0 +1,45 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial scale=1">
+		<link rel="stylesheet" href="style.css">
+		<title>Image Gallery</title>
+	</head>
+	<body>
+		<div class="banner">
+			<div class="navbar">
+				<a href="home.php"><img src="Pictures/logo.png"class="logo"><a>
+				<ul>
+					<li> <a href="home.php">Home</a></li>
+					<li> <a href="menu.php">Menu</a></li>
+					<li> <a href="image.php">Image Gallery</a></li>
+					<li> <a href="aboutus.php">About Us</a></li>
+					<li> <a href="contactus.php">Contact Us</a></li>
+				</ul>
+			</div>
+
+		<div class="col">
+		
+			<div class="card card1"></div>
+			
+			<div class="card card2"></div>
+				
+			<div class="card card3"></div>
+			
+			<div class="card card4"></div>
+			
+			<div class="card card5"></div>
+			
+			<div class="card card6"></div>
+			
+			<div class="card card7"></div>
+			
+			<div class="card card8"></div>
+			
+			<div class="card card9"></div>
+			
+			<div class="card card10"></div>
+		</div>
+	
+	</body>
+</html>
