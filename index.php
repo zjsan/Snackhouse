57773d604f63e6dfd6a5d4f9aae7616dcc108738
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-06-10 11:05:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial scale=1">
+		<link rel="stylesheet" href="style.css">
+		<title>Index</title>
+	</head>
+	<body>
+		<div class="banner">
+		
+			<img src="Pictures/logo.png" alt="logo" class="indexlogo">
+			
+			<div class="indexcontent">
+			
+				<p>WELCOME TO</p>
+				<h1>SAND DUNES SNACK HOUSE</h1>
+				
+				<div class="button">
+					<a href="login.php"><button type="button">LOGIN</button></a>
+					<a href="sign-in.php"><button tySpe="button">SIGN-IN</button></a>
+				</div>
+				
+				<h5>For First Timers kindly register and create your account by clicking sign-in</h5>
+				<h5>For Admin access kindly login and enter your credentials</h5>
+				
+			</div>
+			
+		</div>
+		
+	</body>
+</html>
